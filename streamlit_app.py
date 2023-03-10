92f10a020bc1f7e8f0a2b26ba1616faa1931d2f2
msjiten <127549665+msjiten@users.noreply.github.com>
2023-03-10 16:02:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title ('My parents new healthy dinner')
 
-streamlit.header ('My breakfast menu')
-
-streamlit.text ('oat meal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
